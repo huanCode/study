4212db14ed3f056e08513d8d7a1b16e761c6f9cb
huanCode <403705913@qq.com>
2021-08-19 09:31:49 +0800

    1

--- 数据结构和算法/code/DemoStudy/DemoStudy/DemoStudy.cpp
+++ 数据结构和算法/code/DemoStudy/DemoStudy/DemoStudy.cpp
@@ -12,13 +12,42 @@
 #include "InsertSort.h"
 #include "AvlTreeTmp.h"
 #include "LeetCodeAllSample.h"
-#include "tmp.h"
+//#include "tmp.h"
 using namespace std;
+
+int uniquePaths(int m, int n) {
+	vector<vector<int>> vc(m,vector<int>(n,0));
+	for (int i = 0; i < vc.size(); i++)
+	{
+		vc[i][0] = 1;
+	}
+
+	for (int i = 0; i < vc[0].size(); i++)
+	{
+		vc[0][i] = 1;
+	}
+
+	int valueA = 0;
+	int valueB = 0;
+	for (int i = 1; i < vc.size(); i++)
+	{
+		for (int j = 1; j < vc[0].size(); j++)
+		{
+			valueA = vc[i - 1][j];
+			valueB = vc[i][j - 1];
+			vc[i][j] = valueA + valueB;
+		}
+
+	}
+
+	return vc[m - 1][n - 1];
+}
+
 
 
 int main()
 {
-
+	uniquePaths(3,2);
 	PacificAtlantic_M p;
 	p.Demo();
 
